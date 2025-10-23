f4c9d6bdc4f3ed455608101b5493fc71f3d70b41
sabreenmun <sabreenyo@gmail.com>
2025-10-23 16:35:26 -0400

    Updated num again!

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,6 +1,6 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num = 20;  // Predefined number to calculate factorial
+        int num = 2;  // Predefined number to calculate factorial
         int factorial = 1;
 
         for (int i = 1; i <= num; i++) { // Loop through numbers
